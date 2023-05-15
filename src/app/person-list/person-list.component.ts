5b897a535b3300804dd740dbe6165d18c03a5e71
SyedJawwad <JAWWAD137211@GMAIL.COM>
2023-05-15 08:13:28 -0700

    add peron list on locall storage

--- src/app/person-list/person-list.component.ts
+++ src/app/person-list/person-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { personDto } from '../Model/personDto';
 import { AuthService } from '../Auth/auth.service';
 import { SecurityService } from '../services/security.service';
@@ -9,24 +9,38 @@ import { UserType } from '../enums/user.enum';
   templateUrl: './person-list.component.html',
   styleUrls: ['./person-list.component.css']
 })
-export class PersonListComponent {
-
-  constructor(private authService: AuthService, public securityService: SecurityService) {}
-
-  // isAdminLoggedIn(): boolean {
-  //   return this.authService.isAdmin('true')
-  // }
+export class PersonListComponent implements OnInit {
   userTypeEnum = UserType;
   personList=new Array<personDto>();
   personObj = new personDto();
   selectedIndex = -1;
+  localItem: string | null | undefined;
+  constructor(private authService: AuthService, public securityService: SecurityService) {}
+ngOnInit(): void {
+  this.localItem = localStorage.getItem("List");
+  if(this.localItem == null){
+
+    this.personList = []
+  }
+  else{
+    this.personList =  JSON.parse(this.localItem);
+  }
+
+}
+  // isAdminLoggedIn(): boolean {
+  //   return this.authService.isAdmin('true')
+  // }
+  
 
 
-saveData(){
-  // localStorage.setItem('userData', JSON.stringify(this.personObj))
+storeData(){
+  // var saveData = JSON.parse(localStorage['saveData'] || null) || {};
 
   if (this.selectedIndex === -1) {
     this.personList.push(this.personObj);
+    // localStorage.setItem('saveData', JSON.stringify(this.personObj))
+    localStorage.setItem("List",JSON.stringify(this.personList));
+   
   } else {
     this.personList[this.selectedIndex] = this.personObj;
     this.selectedIndex = -1;
