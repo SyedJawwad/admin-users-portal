412d83430b26ea34209daf6e60161bda208bf29f
SyedJawwad <JAWWAD137211@GMAIL.COM>
2023-05-15 22:23:33 -0700

    Update app.component.ts
    error in login refresh

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { UserType } from './enums/user.enum';
+import { SecurityService } from './services/security.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'admin-users-portal';
+  user = UserType;
+  private router: any;
+  constructor(
+    private securityService: SecurityService  ) {}
+  ngOnInit():void {
+    
+    this.securityService.securityObject = JSON.parse("userData");
+    if (this.user?.id) {
+
+    } else {
+      this.router.navigateByUrl('./login/login.component');    }
+  }
+    
+  
+  
 }
+
