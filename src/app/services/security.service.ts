8a20b8cf7aa547b0056f43c6086843c8936b018c
SyedJawwad <JAWWAD137211@GMAIL.COM>
2023-05-16 06:05:25 -0700

    Update security.service.ts

--- src/app/services/security.service.ts
+++ src/app/services/security.service.ts
@@ -6,10 +6,25 @@ import { UserType } from '../enums/user.enum';
 })
 export class SecurityService {
   userType: UserType = UserType.Admin;
-
-  constructor() { }
-
-  setUserType(userType: UserType): void {
-    this.userType = userType;
-  }
 }
+//   constructor() { }
+
+//   setUserType(userType: UserType): void {
+//     this.userType = userType;
+//   }
+
+//   getUserType(): UserType {
+//     return this.userType;
+//   }
+// }
+
+
+// import { Injectable } from '@angular/core';
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class SecurityService {
+// userType!: number
+//   constructor() { }
+// }
